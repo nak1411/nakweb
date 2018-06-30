113af83300d3d73565a697b93c742a877a9fe56e
Justin <nak1411@gmail.com>
2018-06-29 17:30:36 -0700

    Added hiketracker project

---
+++ hiketracker/js/hiketracker.js
@@ -0,0 +1,32 @@
+/* global $ alert */
+// Add text from input to list after Enter is pressed
+$('input[name=hike-entry]').keypress(function (event) {
+  if (event.which === 13) {
+    var inputText = $(this).val()
+    if (inputText !== '') {
+      $('.item-list').append('<li><span><i class=image></i></span>' +
+                           inputText + '</li>')
+      $(this).val('')
+    } else {
+      alert('Enter Something')
+    }
+  }
+})
+
+// Add text from input to list afer "+" button is pressed
+$('button').on('click', function () {
+  var inputText = $('input[name=hike-entry]').val()
+  if (inputText !== '') {
+    $('.item-list').append('<li><span><i class=image></i></span>' +
+                           inputText + '</li>')
+    $('input[name=hike-entry]').val('')
+  } else {
+    alert('Enter Something')
+  }
+})
+
+// Delete item off list
+$('.item-list').on('click', '.image', function (event) {
+  event.stopPropagation()
+  $(this).closest('li').remove()
+})
