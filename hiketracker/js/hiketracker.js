b5b300a3e8c4d4f045ba21afc869124cfc121c3d
Justin <nak1411@gmail.com>
2018-07-01 17:18:53 -0700

    Fixed formatting

--- hiketracker/js/hiketracker.js
+++ hiketracker/js/hiketracker.js
@@ -1,32 +1,33 @@
 /* global $ alert */
+
 // Add text from input to list after Enter is pressed
 $('input[name=hike-entry]').keypress(function (event) {
   if (event.which === 13) {
-    var inputText = $(this).val()
+    var inputText = $(this).val();
     if (inputText !== '') {
       $('.item-list').append('<li><span><i class=image></i></span>' +
-                           inputText + '</li>')
-      $(this).val('')
+                           inputText + '</li>');
+      $(this).val('');
     } else {
-      alert('Enter Something')
+      alert('Enter Something');
     }
   }
-})
+});
 
 // Add text from input to list afer "+" button is pressed
 $('button').on('click', function () {
-  var inputText = $('input[name=hike-entry]').val()
+  var inputText = $('input[name=hike-entry]').val();
   if (inputText !== '') {
     $('.item-list').append('<li><span><i class=image></i></span>' +
-                           inputText + '</li>')
-    $('input[name=hike-entry]').val('')
+                           inputText + '</li>');
+    $('input[name=hike-entry]').val('');
   } else {
-    alert('Enter Something')
+    alert('Enter Something');
   }
-})
+});
 
 // Delete item off list
 $('.item-list').on('click', '.image', function (event) {
-  event.stopPropagation()
-  $(this).closest('li').remove()
-})
+  event.stopPropagation();
+  $(this).closest('li').remove();
+});
