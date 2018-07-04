cbd9ae90113cb67cab02de044847a26533f397fe
Justin <nak1411@gmail.com>
2018-07-04 12:17:56 -0700

    Local storage with autosave

--- hiketracker/js/hiketracker.js
+++ hiketracker/js/hiketracker.js
@@ -1,12 +1,16 @@
 /* global $ alert */
 
+//Fetch stored data on page load
+window.addEventListener('load', function (event) {
+  $('.item-list').html(localStorage.getItem('entry'));
+});
+
 // Add text from input to list after Enter is pressed
 $('input[name=hike-entry]').keypress(function (event) {
   if (event.which === 13) {
-    var inputText = $(this).val();
+    inputText = $(this).val();
     if (inputText !== '') {
-      $('.item-list').append('<li><span><i class=image></i></span>' +
-        inputText + '</li>');
+      $('.item-list').append('<li><span><i class=image></i></span>' + inputText + '</li>');
       $(this).val('');
     } else {
       alert('Enter Something');
@@ -31,3 +35,8 @@ $('.item-list').on('click', '.image', function (event) {
   event.stopPropagation();
   $(this).closest('li').remove();
 });
+
+//Autosave entered data
+setInterval(function () {
+  localStorage.setItem('entry', $('.item-list').html());
+}, 5000);
