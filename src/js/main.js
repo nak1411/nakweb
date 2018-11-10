e0134d424f8a6a6fe283f0e9873a7588db59f3a9
Justin <nak1411@gmail.com>
2018-11-09 19:22:06 -0800

    Implemented canvas background

--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,19 @@ require('jquery-ui-bundle');
 let APP = (function () {
 
   const init = () => {
+    // Set up canvas
+    let canvas = document.querySelector('.site__canvas');
+    let context = canvas.getContext('2d');
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+
+    // Enable draggable elements
     $(".draggable").draggable({
       snap: true,
       containment: "document"
     });
 
+    // Keep focused draggable elements on top and in correct z-order
     let prev = false;
     $(".draggable").click(function () {
       $(this).css("position", "relative");
