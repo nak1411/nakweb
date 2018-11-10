3e3e84c079df308e1e776de876016f31cfc9003b
Justin <nak1411@gmail.com>
2018-11-09 20:29:40 -0800

    Added particles

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,28 @@ require('jquery');
 require('jquery-ui-bundle');
 
 let APP = (function () {
+  let running = false;
+  let canvas;
+  let context;
+  let particles = [];
+  let maxP = 5000
 
+  /**
+   * Initialize Stuff
+   */
   const init = () => {
     // Set up canvas
-    let canvas = document.querySelector('.site__canvas');
-    let context = canvas.getContext('2d');
+    canvas = document.querySelector('.site__canvas');
+    context = canvas.getContext('2d');
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
+    let screenWidth = canvas.width;
+    let screenHeight = canvas.height;
+
+    // Fill particle array
+    for (let i = 0; i < maxP; i++) {
+      particles.push(new Particle(screenWidth, screenHeight));
+    }
 
     // Enable draggable elements
     $(".draggable").draggable({
@@ -33,6 +48,45 @@ let APP = (function () {
       this.style.zIndex = 1000;
       prev = this;
     });
+
+    // Start loop
+    if (running) {
+      return;
+    } else {
+      running = true;
+      run();
+    }
+  }
+
+  /**
+   * Main Loop
+   */
+  const run = () => {
+    if (running) {
+      requestAnimationFrame(run);
+      render();
+    }
+  }
+
+  /**
+   * Rendering
+   */
+  const render = () => {
+    // Clear screen
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (const particle of particles) {
+      context.fillStyle = '#222';
+      context.fillRect(particle.x, particle.y, 2, 2);
+    }
+  }
+
+  class Particle {
+    constructor(screenWidth, screenHeight) {
+      this.x = Math.floor(Math.random() * screenWidth);
+      this.y = Math.floor(Math.random() * screenHeight);
+    }
   }
   return {
     init: init
