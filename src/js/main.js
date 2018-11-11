abab08c83036ddc5d53733a400e74154ee74dd3b
Justin <nak1411@gmail.com>
2018-11-10 19:09:20 -0800

    Added moving particles with collision

--- src/js/main.js
+++ src/js/main.js
@@ -13,9 +13,7 @@ let APP = (function () {
   let canvas;
   let context;
   let particles = [];
-  let maxP = 5000
-  let mouseX;
-  let mouseY;
+  let maxP;
 
   let fps = 30;
   let now;
@@ -27,8 +25,6 @@ let APP = (function () {
    * Initialize Stuff
    */
   const init = () => {
-
-
     // Set up canvas
     canvas = document.querySelector('.site__canvas');
     context = canvas.getContext('2d');
@@ -37,9 +33,15 @@ let APP = (function () {
     let screenWidth = canvas.width;
     let screenHeight = canvas.height;
 
+    if (screenWidth <= 800) {
+      maxP = 1000;
+    } else {
+      maxP = 5000;
+    }
+
     // Fill particle array
     for (let i = 0; i < maxP; i++) {
-      particles.push(new Particle(screenWidth, screenHeight));
+      particles.push(new Particle(context, screenWidth, screenHeight));
     }
 
     // Enable draggable elements
@@ -78,11 +80,20 @@ let APP = (function () {
       delta = now - then;
       if (delta > interval) {
         then = now - (delta % interval);
+        update();
         render();
       }
-      
     }
-    
+  }
+
+  /**
+   * Updating
+   */
+  const update = () => {
+    for (const particle of particles) {
+      particle.update();
+    }
+
   }
 
   /**
@@ -93,21 +104,52 @@ let APP = (function () {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    // Render starfield
     for (const particle of particles) {
-      context.fillStyle = `rgba(${Math.random() * 100}, 50,  ${Math.random() * 150}, ${particle.brightness})`;
-      context.fillRect(particle.x, particle.y, particle.width, particle.height);
+      particle.render();
     }
   }
 
+  /**
+   * Particle class aka stars
+   */
   class Particle {
-    constructor(screenWidth, screenHeight) {
+    constructor(context, screenWidth, screenHeight) {
+      this.context = context;
+      this.screenWidth = screenWidth;
+      this.screenHeight = screenHeight;
       this.x = Math.floor(Math.random() * screenWidth);
       this.y = Math.floor(Math.random() * screenHeight);
+      this.vx = Math.random() * (1 - (-1)) + (-1);
+      this.vy = Math.random() * (1 - (-1)) + (-1);
+      this.pSpeed = .01;
       this.width = 2;
       this.height = 2;
-      this.brightness = Math.random();
+      this.r = Math.random() * 50;
+      this.g = Math.random() * 50;
+      this.b = Math.random() * 150;
+      this.alpha = Math.random() * (1 - (.2)) + (.2);
+    }
+
+    render() {
+      this.context.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, ${this.alpha})`;
+      this.context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+      if (this.x >= this.screenWidth || this.x <= 0) {
+        this.vx = -this.vx;
+      }
+
+      if (this.y >= this.screenHeight || this.y <= 0) {
+        this.vy = -this.vy;
+      }
+
+      this.x += this.vx;
+      this.y += this.vy;
     }
   }
+
   return {
     init: init
   }
